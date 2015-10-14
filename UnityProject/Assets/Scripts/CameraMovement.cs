af0109e55b349b1c7183851d6387c2fbf0b2290b
moisesjbc <moisesjbc@gmail.com>
2015-10-14 10:00:34 +0100

    Added missing CameraMovement script.

---
+++ UnityProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private bool goingForward = true;
+	public LODPlane lodPlane;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (lodPlane,new Vector3(0,4,-5),Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		const float step = 0.015f;
+		
+		if( goingForward ){
+			transform.Translate (step * Vector3.forward);
+			if( transform.position.y < 5.0f ){
+				goingForward = false;
+			}
+		}else{
+			transform.Translate (-step * Vector3.forward);
+			if( transform.position.y > 10.0f ){
+				goingForward = true;
+			}
+		}
+	}
+}
