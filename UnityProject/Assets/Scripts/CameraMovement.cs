d91f7f421eed28f42a9f23db9b294f49b2b75a9b
moisesjbc <moisesjbc@gmail.com>
2015-10-19 11:25:55 +0100

    Now velocity and orientation is controlled with Oculus.

--- UnityProject/Assets/Scripts/CameraMovement.cs
+++ UnityProject/Assets/Scripts/CameraMovement.cs
@@ -1,30 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.VR;
+
 
 public class CameraMovement : MonoBehaviour {
 	bool mouseLeftButtonPressed = false;
 	float velocity = 1.0f;
 
-	// Update is called once per frame
-	void Update () {
+
+	void LateUpdate () {
 		const float zoomStep = 1.0f;
 		const float translationStep = 0.5f;
-
-		// Allow user to zoom in and out with the mouse whell.
-		float mouseZoom = Input.GetAxis ("Mouse ScrollWheel");
-		transform.Translate ( mouseZoom * zoomStep * Vector3.forward);
 		
-		// Allow user to move over the map by moving the mouse.
-		if (Input.GetMouseButtonDown (0)) {
-			mouseLeftButtonPressed = true;
-		} else if( Input.GetMouseButtonUp(0)) {
-			mouseLeftButtonPressed = false;
-		} else if (mouseLeftButtonPressed) {
-			transform.Translate (Input.GetAxis ("Mouse Y") * -translationStep * Vector3.up);
-			transform.Translate (Input.GetAxis ("Mouse X") * -translationStep * Vector3.right);
-		}
+		if (VRSettings.enabled) {
+			// Keep moving the player forward.
+			transform.position += GetComponent<OVRCameraRig> ().centerEyeAnchor.rotation * (velocity * 0.001f * transform.position.y * Vector3.forward);
 
-		// Keep moving the player forward.
-		transform.Translate (velocity * 0.001f * transform.position.y * Vector3.forward);
+			// Increase or decrease velocity with Oculus touchpad.
+			if (Input.GetMouseButtonDown (0)) {
+				mouseLeftButtonPressed = true;
+			} else if (Input.GetMouseButtonUp (0)) {
+				mouseLeftButtonPressed = false;
+			} else {
+				if (Input.GetAxis ("Mouse X") > 0.0f) {
+					velocity -= 1.0f;
+					mouseLeftButtonPressed = false;
+				} else if (Input.GetAxis ("Mouse X") < 0.0f) {
+					velocity += 1.0f;
+					mouseLeftButtonPressed = false;
+				}
+			}
+		} else {
+			// Allow user to zoom in and out with the mouse whell.
+			float mouseZoom = Input.GetAxis ("Mouse ScrollWheel");
+			transform.Translate (mouseZoom * zoomStep * Vector3.forward);
+			
+			// Allow user to move over the map by moving the mouse.
+			if (Input.GetMouseButtonDown (0)) {
+				mouseLeftButtonPressed = true;
+			} else if (Input.GetMouseButtonUp (0)) {
+				mouseLeftButtonPressed = false;
+			} else if (mouseLeftButtonPressed) {
+				transform.Translate (Input.GetAxis ("Mouse Y") * -translationStep * Vector3.up);
+				transform.Translate (Input.GetAxis ("Mouse X") * -translationStep * Vector3.right);
+			}
+		}
 	}
 }
