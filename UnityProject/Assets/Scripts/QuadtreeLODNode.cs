c08952b911beb6e8ff110c091591e566eb43e800
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2015-10-23 09:24:26 +0100

    Now WCS requests are performed using dimensions instead of res.

--- UnityProject/Assets/Scripts/QuadtreeLODNode.cs
+++ UnityProject/Assets/Scripts/QuadtreeLODNode.cs
@@ -333,22 +333,17 @@ public class QuadtreeLODNode {
 	                                    Vector2 bottomLeftCoordinates, 
 	                                    Vector2 topRightCoordinates,
 	                                    int N ){
-		Vector2 wcsResolution = new Vector2 (
-			(topRightCoordinates.x - bottomLeftCoordinates.x) / N,
-			(topRightCoordinates.y - bottomLeftCoordinates.y) / N
-		);
-
 		string fixedUrl = "http://www.idee.es/wcs/IDEE-WCS-UTM28N/wcsServlet?REQUEST=GetCoverage&SERVICE=WCS&VERSION=1.0.0&FORMAT=AsciiGrid&COVERAGE=MDT_canarias&CRS=EPSG:25828&REFERER=CAPAWARE";
 		string bboxUrlQuery = 
 			"&BBOX=" + bottomLeftCoordinates.x + "," +
 			bottomLeftCoordinates.y + "," +
 			topRightCoordinates.x + "," +
 			topRightCoordinates.y;
-		string resolutionUrlQuery =
-			"&RESX=" + wcsResolution.x +
-			"&RESY=" + wcsResolution.y;
+		string dimensionsUrlQuery =
+			"&WIDTH=" + N +
+			"&HEIGHT=" + N;
 
-		string url = fixedUrl + bboxUrlQuery + resolutionUrlQuery;
+		string url = fixedUrl + bboxUrlQuery + dimensionsUrlQuery;
 
 		Debug.Log ("heightMap URL - " + url);
 
