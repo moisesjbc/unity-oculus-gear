38f332f584730a9a869b1ce45f4cc19ca786f8cf
moisesjbc <moisesjbc@gmail.com>
2015-10-28 10:26:08 +0000

    Added pickup destruction when collided.

---
+++ UnityProject/Assets/Scripts/PickupCollection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupCollection : MonoBehaviour {
+
+	void OnTriggerEnter( Collider other )
+	{
+		if (other.gameObject.tag == "Pickup") {
+			Destroy (other.gameObject);
+			Debug.Log ("Pickup!");
+		}
+	}
+}
