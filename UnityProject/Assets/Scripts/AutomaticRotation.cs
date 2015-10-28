fe0a8e92598f49ce1441d1f53fe9367ad4274430
moisesjbc <moisesjbc@gmail.com>
2015-10-28 10:18:07 +0000

    Added an rotating cube named "Pickup".

---
+++ UnityProject/Assets/Scripts/AutomaticRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutomaticRotation : MonoBehaviour {
+	
+	void Update () 
+	{
+		transform.Rotate (0.0f, 75.0f * Time.deltaTime, 0.0f);
+	}
+}
