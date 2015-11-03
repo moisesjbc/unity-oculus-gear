0682fbb68666c8dfce78342c152631b6c8edd89b
moisesjbc <moisesjbc@gmail.com>
2015-11-03 10:46:57 +0000

    Added score reset to level reset.

--- UnityProject/Assets/Scripts/PickupCollection.cs
+++ UnityProject/Assets/Scripts/PickupCollection.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class PickupCollection : MonoBehaviour {
 
-	public int score = 0;
+	private int score = 0;
 
-	void OnTriggerEnter( Collider other )
+
+	public void Reset()
+	{
+		score = 0;
+	}
+
+
+	public int Score()
+	{
+		return score;
+	}
+
+
+	public void OnTriggerEnter( Collider other )
 	{
 		if (other.gameObject.tag == "Pickup") {
 			Destroy (other.transform.parent.gameObject);
